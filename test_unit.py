70e99e698514a969f1c692cfcc91cbae0670ab89
GreenyWind <whelzyq@163.com>
2020-03-07 21:22:11 -0500

    Add files via upload

---
+++ test_unit.py
@@ -0,0 +1,6 @@
+from twiiter_video_generator import workflow
+
+def test_unit():
+	keys = ['preselect', 'coronavirus', 'tornado', 'face+mask', 'kitty', 'puppy', 'punderworld', 'watercolor'
+        , 'Louvre', 'Mecca', 'Dows', 'Bill+Gates', 'quit']
+    workflow(keys)
