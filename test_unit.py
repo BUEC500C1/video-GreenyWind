39c6c5e1b7de4f970a556fba03d10c4f12ebe3f7
GreenyWind <whelzyq@163.com>
2020-03-08 16:34:05 -0400

    Add files via upload

--- test_unit.py
+++ test_unit.py
@@ -3,4 +3,4 @@ from twitter_video_generator import workflow
 def test_pressure_test():
     keys = ['preselect', 'coronavirus', 'tornado', 'face+mask', 'kitty', 'puppy', 'punderworld', 'watercolor'
         , 'Louvre', 'Mecca', 'Dows', 'Bill+Gates', 'quit']
-    workflow(keys)
+    assert workflow(keys) == True
